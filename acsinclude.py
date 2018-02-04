249eb6abe135f412ebd240f3520f82b53688cb4a
Alexander Korshun <Korshun@users.noreply.github.com>
2018-02-04 18:32:27 +0300

    acsinclude improvements

--- acsinclude.py
+++ acsinclude.py
@@ -3,41 +3,16 @@
 import os
 import sys
 import re
+import io
+from sys import argv, exit
  
-ignored = { 'zcommon.acs' }
+def readfile(filename, mode='r'):
+    with open(filename, mode) as f:
+        return f.read()
+ 
 included = set()
-includepath = []
- 
-def main():
-    global out
-    global includepath
-    if len(sys.argv) < 3:
-        print('Usage: acsinclude <input> <output> [include directories]')
-        sys.exit(2)
- 
-    with open(sys.argv[2], 'w') as o:
-        out = o
-        includepath = [os.path.dirname(sys.argv[1])] + sys.argv[3:]
-        include_file(os.path.basename(sys.argv[1]))
- 
-def include_file(filename):
-    if filename in ignored:
-        return
-   
-    if filename in included:
-        print('ERROR: {} included twice'.format(filename))
-        sys.exit(1)
-    included.add(filename)
- 
-    text = readfile(resolve_include(filename))
-    offset = 0
-    for match in re.finditer(r'#include "(.*?)"', text):
-        out.write(text[offset:match.start()])
-        offset = match.end()
-        include_file(match.group(1))
- 
-    out.write(text[offset:])
- 
+includepath = [ '.' ]
+
 def resolve_include(filename):
     for dir in includepath:
         filepath = os.path.join(dir, filename)
@@ -45,11 +20,38 @@ def resolve_include(filename):
             return filepath
  
     print('ERROR: {} not found in include path'.format(filename))
-    sys.exit(1)
+    exit(1)
+
+def include_file(filename, out):
+    if filename.lower() == 'zcommon.acs':
+        return
+   
+    if filename in included:
+        print('ERROR: {} included twice'.format(filename))
+        exit(1)
+    included.add(filename)
  
-def readfile(filename, mode='r'):
-    with open(filename, mode) as f:
-        return f.read()
+    text = readfile(resolve_include(filename))
+    offset = 0
+    for match in re.finditer(b'#include[ \t]+"(.*?)"', text, re.IGNORECASE):
+        out.write(text[offset:match.start()])
+        offset = match.end()
+        include_file(match.group(1).decode('utf-8'), out)
  
+    out.write(text[offset:])
+ 
+def main(): 
+    if len(argv) < 3:
+        print('Usage: acsinclude <input> <output> [include directories]')
+        exit(2)
+ 
+    global includepath
+    includepath = [os.path.dirname(argv[1])] + argv[3:]
+ 
+    text = io.StringIO()
+    include_file(os.path.basename(argv[1]), text)
+
+    
+    
 if __name__ == '__main__':
     main()
